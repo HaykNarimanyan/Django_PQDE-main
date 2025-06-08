116456b3fab267ae4633012f2deb2b38a95a5bb7
HaykNarimanyan <hayknarimanyan777@gmail.com>
2025-06-08 19:37:19 +0400

    Add files via upload

---
+++ Django_PQDE-main/README.md
@@ -0,0 +1,2 @@
+# Django_PQDE
+v1
---
+++ Django_PQDE-main/core/main/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Post
+from .forms import PostForm
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'main/post_list.html', {'posts': posts})
+
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('post_list')
+    else:
+        form = PostForm()
+    return render(request, 'main/post_form.html', {'form': form})
+
+def edit_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('post_list')
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'main/post_form.html', {'form': form})
+
+def delete_post(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('post_list')
+    return render(request, 'delete_post.html', {'post': post})
